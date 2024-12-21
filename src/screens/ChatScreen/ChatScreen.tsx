896c0ca19bf4244a57a50a0de2f40433862611f5
pawanbondre67 <bondrepavan623@gmail.com>
2024-12-21 18:16:15 +0530

    initial commit

--- src/screens/ChatScreen/ChatScreen.tsx
+++ src/screens/ChatScreen/ChatScreen.tsx
@@ -8,11 +8,26 @@ import React ,
 } from 'react';
 import { TouchableOpacity,Text, Alert } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 // import { AntDesign } from '@expo/vector-icons';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, IMessage } from 'react-native-gifted-chat';
 import { useNavigation } from '@react-navigation/native';
+import { useFirebaseUser } from '../../hooks/useFirebaseUser';
+
+const uuidv4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+      const r = Math.floor(Math.random() * 16);
+      const v = c === 'x' ? r : (r % 4) + 8;
+      return v.toString(16);
+    });
+  };
+
+
 const ChatScreen = () => {
+    
     const navigation = useNavigation();
+    const firebaseUser =useFirebaseUser().firebaseUser;
+    const groupId = '1234567';
     const onSignOut = () => {
         auth().signOut().catch((error) => Alert.alert(error.message));
         navigation.navigate('auth');
@@ -30,20 +45,64 @@ const ChatScreen = () => {
         });
     }, [navigation]);
 
-    const [messages, setMessages] = useState([]);
+    useLayoutEffect(() => {
+        const unsubscribe = firestore()
+        .collection('groups')
+        .doc(groupId)
+        .collection('messages')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(querySnapshot => {
+          const messagesFirestore = querySnapshot.docs.map(doc => {
+    
+  
+            const data = {
+              _id: doc.id,
+              text: '',
+              createdAt: new Date().getTime(),
+              text : doc.data().text,
+              user : doc.data().user,
+              
+            };
+  
+            return data;
+          });
+  
+          setMessages(messagesFirestore);
+        });
+  
+      return () => unsubscribe();
+    }
+    , [groupId]);
+
+
+    const [messages, setMessages] = useState<IMessage []>([]);
     const onSend = (newMessages = []) => {
         setMessages(GiftedChat.append(messages, newMessages));
+        const text = messages[0].text;
+        firestore()
+          .collection('groups')
+          .doc(groupId)
+          .collection('messages')
+          .add({
+            text,
+            createdAt: new Date().getTime(),
+            user: {
+              _id: firebaseUser?.uid,
+              name: firebaseUser?.displayName || 'jack',
+            },
+          });
     };
-
+    
     return (
         <GiftedChat
             messages={messages}
-            onSend={messages => onSend(messages)}
-            user={{
-                _id: 1,
-            }}
+            onSend={onSend}
         />
     );
-}
+
+
+
+};
+
 
 export default ChatScreen
